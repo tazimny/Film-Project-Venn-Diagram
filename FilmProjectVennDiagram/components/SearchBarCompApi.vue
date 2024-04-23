30dc986f6321927954d01bce47f5e5c68dea1981
TomZimny92 <64618507+TomZimny92@users.noreply.github.com>
2024-04-22 20:21:53 -0400

    trying to convert to composition api again

---
+++ FilmProjectVennDiagram/components/SearchBarCompApi.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <input
+            :placeholder="label"
+
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { ref, defineProps, defineEmits } from 'vue'
+    const filteredResults: [] = ref([])
+    const imageUrl = ref('https://image.tmdb.org/t/p/w500/')
+
+    const props = defineProps({
+        label: {
+            type: String,
+            default: 'Enter name...'
+        },
+        inputValue: {
+            type: [String, Number],
+            default: ''
+        }
+    })
+
+    const emit = defineEmits(['update', 'update:modelValue'])
+
+    const selectPerson = async (person: String): Promise<void> => {
+        emit('update:inputValue', person.fullName)
+        filteredResults.value = []
+    }
+</script>
